a1e2b7334ea1e8a5e64cb629c1abe191c710c401
Daniel Nejdmo <dnejdmo@hotmail.com>
2025-01-08 10:52:44 +0100

    removed error

--- Calculator.cs
+++ Calculator.cs
@@ -9,7 +9,7 @@ namespace calc
     public class Calculator
     {
         public int Add(int a, int b)
-        { return a + b + 1; }
+        { return a + b; }
 
         public int Subtract(int a, int b) 
         { return a - b; }
